9053cb6176002e36bf39aa656b854b223943e9c5
BethBurch <beth.burch12@gmail.com>
2023-06-09 17:16:05 -0500

    Browser still not working, all tests pass. SQL Query no longer pissy(problem was OR and Left inner join)

--- queries.sql
+++ queries.sql
@@ -9,6 +9,7 @@ DROP TABLE job;
 
 -- Part 4: Test it with SQL
 SELECT * FROM skill
-LEFT INNER JOIN job_skills ON skill.id = job_skills.skills_id OR job_skills.skills_id = skill.id WHERE job_skills.jobs_id IS NOT NULL
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
 
 ORDER BY name ASC;
